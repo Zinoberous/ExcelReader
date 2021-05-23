70edad11247ff19f8b41663332a659b2b6a51100
Zinoberous <alexanderkarge@outlook.de>
2021-05-23 16:11:20 +0200

    Lokale Pfade aus Tester entfernt

--- ExcelReader.Tester/Program.cs
+++ ExcelReader.Tester/Program.cs
@@ -9,8 +9,7 @@ namespace ExcelReader.Tester
         static void Main(string[] args)
         {
             var excel = new ExcelLoader().LoadAll(new List<string>() {
-                @"D:\Code\Repos\Kartenspiel\Kartenspiel.Tester\Beispiel\Karten.xlsx",
-                @"D:\Code\Repos\Kartenspiel\Kartenspiel.Tester\Beispiel\Decks.xlsx"
+                @""
             });
 
             Console.ReadKey();
